41ed82bfb20f52ac19a731467608fe46c3925be9
Ajay Chauhan <exactlyajay.chauhan@gmail.com>
2024-08-01 13:57:50 +0530

    chore: update build script

--- build.sh
+++ build.sh
@@ -4,18 +4,12 @@
 mkdir -p bin
 
 # Build for Windows (64-bit)
-GOOS=windows GOARCH=amd64 go build -o bin/license-generator.exe ./src
-zip -j bin/license-generator-windows-amd64.zip bin/license-generator.exe
-rm bin/license-generator.exe
+GOOS=windows GOARCH=amd64 go build -o bin/license-generator.exe .
 
 # Build for macOS (64-bit)
-GOOS=darwin GOARCH=amd64 go build -o bin/license-generator ./src
-tar -czf bin/license-generator-darwin-amd64.tar.gz -C bin license-generator
-rm bin/license-generator
+GOOS=darwin GOARCH=amd64 go build -o bin/license-generator-darwin .
 
 # Build for Linux (64-bit)
-GOOS=linux GOARCH=amd64 go build -o bin/license-generator ./src
-tar -czf bin/license-generator-linux-amd64.tar.gz -C bin license-generator
-rm bin/license-generator
+GOOS=linux GOARCH=amd64 go build -o bin/license-generator .
 
-echo "Compressed binaries are in the bin directory."
+echo "Binaries are in the bin directory."
